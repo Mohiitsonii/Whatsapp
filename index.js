552d9b3d4e792d69e45417a5c536caf2aab8bce4
Mohit <Mohitsonims04@gmail.com>
2023-07-29 21:39:02 +0530

    value changed  token fixed

--- index.js
+++ index.js
@@ -76,11 +76,12 @@ app.post("/webhook", (req, res) => {
           messaging_product: "whatsapp",
           to: from,
           text: {
-            body: "Hi.. I'm Prasath, your message is " + msg_body,
+            body: "Hi.. I'm mohit, your message is " + msg_body,
           },
         },
         headers: {
           "Content-Type": "application/json",
+          "Authorization":`Bearer ${api_key}`
         },
       });
 

bed2817fd500ea4cad19318d9109a7d9f07bd3f2
Mohit <Mohitsonims04@gmail.com>
2023-07-29 21:22:00 +0530

    acess token fixed

--- index.js
+++ index.js
@@ -42,34 +42,48 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
+  //i want some
+
   let body_param = req.body;
+
   console.log(JSON.stringify(body_param, null, 2));
+
   if (body_param.object) {
+    console.log("inside body param");
     if (
       body_param.entry &&
       body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.message &&
-      body_param.entry[0].changes[0].value.message[0]
+      body_param.entry[0].changes[0].value.messages &&
+      body_param.entry[0].changes[0].value.messages[0]
     ) {
-      let phn_no_id = body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body.entry[0].changes[0].value.message[0].from;
-      let msg_body = body.entry[0].changes[0].value.message[0].text.body;
+      let phon_no_id =
+        body_param.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = body_param.entry[0].changes[0].value.messages[0].from;
+      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+      console.log("phone number " + phon_no_id);
+      console.log("from " + from);
+      console.log("boady param " + msg_body);
 
       axios({
         method: "POST",
-        url: "https://graph.facebook.com/v17.0/" + phn_no_id + "/messages?acess_token="+api_key,
+        url:
+          "https://graph.facebook.com/v13.0/" +
+          phon_no_id +
+          "/messages?access_token=" +
+          api_key,
         data: {
           messaging_product: "whatsapp",
           to: from,
           text: {
-            body: "Hi....I am mohit ",
+            body: "Hi.. I'm Prasath, your message is " + msg_body,
           },
         },
         headers: {
-          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       });
+
       res.sendStatus(200);
     } else {
       res.sendStatus(404);
